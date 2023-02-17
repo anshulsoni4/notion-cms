19ebc7f8ef8c18360494992840d24754ffcda078
anshulsoni-scrobits <anshulsoni3040@gmail.com>
2023-02-17 12:42:34 +0530

    update

--- functions/fetchNotion.js
+++ functions/fetchNotion.js
@@ -1,19 +1,32 @@
-const { Client } = require("@notionhq/client")
+const { Client } = require('@notionhq/client');
 
 const { NOTIONKEY, NOTIONDB } = process.env;
 
 // Initializing a client
 const notion = new Client({
     auth: NOTIONKEY,
-  })
+});
 
 exports.handler = async function (event, context) {
-    const response = await notion.databases.query({
-        database_id: NOTIONDB,
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response),
-    };
-  };
-  
+    try {
+        const response = await notion.databases.query({
+            database_id: NOTIONDB,
+            filter: {
+                property: 'Status',
+                select: {
+                    equals: 'Live',
+                },
+            },
+        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response),
+        };
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: e.toString(),
+        };
+    }
+};
